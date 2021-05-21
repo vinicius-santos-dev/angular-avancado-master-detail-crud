f3e53a89e0b7a97517439d5b28b8ee16ab2f3873
viniciusssantos <vinicius.santos@lejour.com.br>
2021-05-21 14:27:07 -0300

    Setting up EntriesModule

---
+++ src/app/pages/entries/shared/entry.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
+
+import { Entry } from './entry.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EntryService {
+  private apiPath: string = 'api/entries';
+
+  constructor(private http: HttpClient) {}
+
+  public getAll(): Observable<Entry[]> {
+    return this.http
+      .get(this.apiPath)
+      .pipe(catchError(this.handleError), map(this.jsonDataToEntries));
+  }
+
+  public getById(id: number): Observable<Entry> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http
+      .get(url)
+      .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+  }
+
+  public create(entry: Entry): Observable<Entry> {
+    return this.http
+      .post(this.apiPath, entry)
+      .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+  }
+
+  public update(entry: Entry): Observable<Entry> {
+    const url = `${this.apiPath}/${entry.id}`;
+
+    return this.http.put(url, entry).pipe(
+      catchError(this.handleError),
+      map(() => entry)
+    );
+  }
+
+  public delete(id: number): any {
+    const url = `${this.apiPath}/${id}`;
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+  // PRIVATE METHODS
+
+  private jsonDataToEntry(jsonData: any): Entry {
+    return jsonData as Entry;
+  }
+
+  private jsonDataToEntries(jsonData: any[]): Entry[] {
+    const entries: Entry[] = [];
+
+    jsonData.forEach((element) => entries.push(element as Entry));
+    return entries;
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.log('ERRO NA REQUISIÇÃO => ', error);
+    return throwError(error);
+  }
+}
