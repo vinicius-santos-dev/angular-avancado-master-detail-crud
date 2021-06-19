a16496dfb8f6836f30682467faa9a438fe3cc5a8
viniciusssantos <vinicius.santos@lejour.com.br>
2021-06-19 16:32:14 -0300

    Adding association between Entry and Category

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -5,6 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map, catchError, flatMap } from 'rxjs/operators';
 
 import { Entry } from './entry.model';
+import { CategoryService } from '../../categories/shared/category.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,34 +13,40 @@ import { Entry } from './entry.model';
 export class EntryService {
   private apiPath: string = 'api/entries';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private categoryService: CategoryService) {}
 
   public getAll(): Observable<Entry[]> {
-    return this.http
-      .get(this.apiPath)
-      .pipe(catchError(this.handleError), map(this.jsonDataToEntries));
+    return this.http.get(this.apiPath).pipe(catchError(this.handleError), map(this.jsonDataToEntries));
   }
 
   public getById(id: number): Observable<Entry> {
     const url = `${this.apiPath}/${id}`;
 
-    return this.http
-      .get(url)
-      .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+    return this.http.get(url).pipe(catchError(this.handleError), map(this.jsonDataToEntry));
   }
 
   public create(entry: Entry): Observable<Entry> {
-    return this.http
-      .post(this.apiPath, entry)
-      .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap((category) => {
+        entry.category = category;
+
+        return this.http.post(this.apiPath, entry).pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+      })
+    );
   }
 
   public update(entry: Entry): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
 
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry)
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap((category) => {
+        entry.category = category;
+
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+        );
+      })
     );
   }
 
