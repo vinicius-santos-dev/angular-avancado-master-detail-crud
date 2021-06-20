90a0970c3624a736be4bf7865b9035da256b573f
viniciusssantos <vinicius.santos@lejour.com.br>
2021-06-20 03:36:26 -0300

    Adding BaseResourceModel

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,7 +1,7 @@
-export class Category {
-  constructor(
-    public id?: number,
-    public name?: string,
-    public description?: string
-  ) {}
+import { BaseResourceModel } from '../../../shared/models/base-resource.model';
+
+export class Category extends BaseResourceModel {
+  constructor(public id?: number, public name?: string, public description?: string) {
+    super();
+  }
 }
