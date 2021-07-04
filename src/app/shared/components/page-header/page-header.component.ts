ec65f86b3a44eee162723c4d877778a46a3742e6
viniciusssantos <vinicius.santos@lejour.com.br>
2021-07-04 16:04:11 -0300

    Adding PageHeaderComponent to pages

---
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.css']
+})
+export class PageHeaderComponent implements OnInit {
+  @Input() public pageTitle: string;
+  @Input() public buttonClass: string;
+  @Input() public buttonText: string;
+  @Input() public buttonLink: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [BreadcrumbComponent],
-  imports: [CommonModule, ReactiveFormsModule],
-  exports: [CommonModule, ReactiveFormsModule, BreadcrumbComponent],
+  declarations: [BreadcrumbComponent, PageHeaderComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  exports: [CommonModule, ReactiveFormsModule, RouterModule, BreadcrumbComponent, PageHeaderComponent],
 })
 export class SharedModule {}
