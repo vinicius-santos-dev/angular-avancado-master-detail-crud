9eafa4b74a84d7a7d0fec5ab1214a7c0170db46c
viniciusssantos <vinicius.santos@lejour.com.br>
2021-07-04 18:23:43 -0300

    Adding Reports base

---
+++ src/app/pages/reports/reports.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+
+import { ReportsRoutingModule } from './reports-routing.module';
+import { ReportsComponent } from './reports/reports.component';
+
+@NgModule({
+  declarations: [ReportsComponent],
+  imports: [SharedModule, ReportsRoutingModule],
+  exports: [],
+})
+export class ReportsModule {}
