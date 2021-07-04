7483bc5d7bd963ed4305e4763b818bf4bbe57e04
viniciusssantos <vinicius.santos@lejour.com.br>
2021-07-04 15:01:14 -0300

    Adding BreadcrumbComponent

---
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+interface BreadCrumbItem {
+  text: string;
+  link?: string;
+}
+
+@Component({
+  selector: 'app-breadcrumb',
+  templateUrl: './breadcrumb.component.html',
+  styleUrls: ['./breadcrumb.component.css'],
+})
+export class BreadcrumbComponent implements OnInit {
+  @Input() public items: BreadCrumbItem[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public isTheLastItem(item: BreadCrumbItem): boolean {
+    const index = this.items.indexOf(item);
+
+    return index === this.items.length - 1;
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [BreadcrumbComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [CommonModule, ReactiveFormsModule],
+  exports: [CommonModule, ReactiveFormsModule, BreadcrumbComponent],
 })
 export class SharedModule {}
