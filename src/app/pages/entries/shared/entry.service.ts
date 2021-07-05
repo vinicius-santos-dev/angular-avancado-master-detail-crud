7c07bdfd115894709d50f2af903610f27601fc96
viniciusssantos <vinicius.santos@lejour.com.br>
2021-07-04 21:55:39 -0300

    Improving reports

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, Injector } from '@angular/core';
 
-import { Entry } from './entry.model';
-import { CategoryService } from '../../categories/shared/category.service';
-import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
-
 import { Observable } from 'rxjs';
-import { catchError, flatMap } from 'rxjs/operators';
+import { flatMap, catchError, map } from 'rxjs/operators';
+
+import { BaseResourceService } from '../../../shared/services/base-resource.service';
+import { CategoryService } from '../../categories/shared/category.service';
+import { Entry } from './entry.model';
+
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root',
@@ -15,23 +17,35 @@ export class EntryService extends BaseResourceService<Entry> {
     super('api/entries', injector, Entry.fromJson);
   }
 
-  public create(entry: Entry): Observable<Entry> {
+  create(entry: Entry): Observable<Entry> {
     return this.setCategoryAndSendToServer(entry, super.create.bind(this));
   }
 
-  public update(entry: Entry): Observable<Entry> {
+  update(entry: Entry): Observable<Entry> {
     return this.setCategoryAndSendToServer(entry, super.update.bind(this));
   }
 
-  // PRIVATE METHODS
+  getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+    return this.getAll().pipe(map((entries) => this.filterByMonthAndYear(entries, month, year)));
+  }
+
   private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap((category) => {
         entry.category = category;
-
         return sendFn(entry);
       }),
       catchError(this.handleError)
     );
+  }
+
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+    return entries.filter((entry) => {
+      const entryDate = moment(entry.date, 'DD/MM/YYYY');
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+
+      if (monthMatches && yearMatches) return entry;
+    });
   }
 }
--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -4,9 +4,11 @@ import { SharedModule } from '../../shared/shared.module';
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports/reports.component';
 
+import { ChartModule } from 'primeng/chart';
+
 @NgModule({
   declarations: [ReportsComponent],
-  imports: [SharedModule, ReportsRoutingModule],
+  imports: [SharedModule, ReportsRoutingModule, ChartModule],
   exports: [],
 })
 export class ReportsModule {}
