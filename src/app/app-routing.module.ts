7bfb7ca2d47ea507ce4e1aa43078eecd93d265bf
viniciusssantos <vinicius.santos@lejour.com.br>
2021-07-04 22:00:55 -0300

    Adding default blank route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/reports',
+    pathMatch: 'full',
+  },
+  {
     path: 'categories',
     loadChildren: () => import('./pages/categories/categories.module').then((m) => m.CategoriesModule),
   },
